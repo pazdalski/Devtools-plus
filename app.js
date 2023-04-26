17944fea34557804386ad2063e46ddcc1bdd5598
pazdalski <konrad3021@gmail.com>
2023-04-26 12:25:34 +0200

    Less code, more intuitive

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 const inputs = document.querySelectorAll("input");
 const ui = document.querySelector("#INTERFACE");
 const title = document.querySelector("#title");
-const copyBtn = document.querySelector("#copy");
+const copyBoxShadowBtn = document.querySelector("#box-shadow");
 
 const values = {
   // Default values
   boxShadow: {
-    horizontal: 8,
-    vertical: 8,
+    horizontal: 0,
+    vertical: 0,
     blur: 24,
     spread: 0,
     color: `#00000050`,
     inset: ``,
   },
 };
+/* --------------------------------- Logic --------------------------------- */
+
 inputs.forEach((input) => {
   input.addEventListener("input", () => {
     const id = input.id;
     const legend = document.querySelector(`legend[id="${id}"]`);
-    legend.innerHTML = `${legend.getAttribute("id")} ${input.value}`;
+    legend.innerHTML = `${legend.getAttribute("id")} <b class="orange">${
+      input.value
+    }</b>`;
     switch (id) {
       // Box-shadow
       case "horizontal":
@@ -37,8 +41,9 @@ inputs.forEach((input) => {
         values.boxShadow.color = input.value;
         break;
       case "inset":
-        if (values.inset == "inset") {
+        if (values.boxShadow.inset == "inset") {
           values.boxShadow.inset = "";
+          legend.innerHTML = `${legend.getAttribute("id")}`;
         } else {
           values.boxShadow.inset = "inset";
         }
@@ -48,22 +53,21 @@ inputs.forEach((input) => {
     setShadow();
   });
 });
+
+//# Box shadow
 function setShadow() {
   document.documentElement.style.setProperty(
-    "--ebs",
+    "--DTBoxShadow",
     `${values.boxShadow.horizontal}px ${values.boxShadow.vertical}px ${values.boxShadow.blur}px ${values.boxShadow.spread}px ${values.boxShadow.color} ${values.boxShadow.inset}`
   );
 }
-
-copyBtn.addEventListener("click", () => {
-  const code = `box-shadow: ${values.boxShadow.horizontal}px ${values.boxShadow.vertical}px ${values.boxShadow.blur}px ${values.boxShadow.spread}px ${values.boxShadow.color} ${values.inset};`;
+//? Copy button
+copyBoxShadowBtn.addEventListener("click", () => {
+  const code = `box-shadow: ${values.boxShadow.horizontal}px ${values.boxShadow.vertical}px ${values.boxShadow.blur}px ${values.boxShadow.spread}px ${values.boxShadow.color} ${values.boxShadow.inset};`;
   navigator.clipboard.writeText(code); // Clipboard
-  copyBtn.style.border = "1px solid #2fd4a8";
-  setTimeout(() => {
-    copyBtn.style.border = "";
-  }, 200);
 });
 
+//? Drag and drop
 title.addEventListener("drag", (e) => {
   e.preventDefault();
   const dimensions = ui.getBoundingClientRect();
